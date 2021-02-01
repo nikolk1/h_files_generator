0785dcb80fae2d1f71eb8f0773882433210595c8
nikolk1 <nikolk202@gmail.com>
2021-02-01 08:37:38 +0200

    Change main to be more comfortable cli tool

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
+extern crate clap;
+
 use header_constructor::header_constructor::HeaderConstructor;
-use std::env;
+use clap::Clap;
 
 mod header_constructor;
+
+#[derive(Clap)]
+#[clap(version = "1.0")]
+struct Opts {
+    #[clap(short, long)]
+    includes: bool,
+    #[clap(short, long)]
+    defines: bool,
+    #[clap(short, long)]
+    structs: bool,
+    #[clap(long)]
+    path: String
+}
 
 fn main() {
     // let args: Vec<String> = env::args().collect();
     // println!("Hello, world!");
+    let opts: Opts = Opts::parse();
 
-    let mut h = HeaderConstructor::new("/home/nikol/Documents/h_generator/src/test.c");
-    h.generate_header(true, true, true);
+    let mut h = HeaderConstructor::new(&opts.path);
+    h.generate_header(opts.includes, opts.structs, opts.defines);
 }
